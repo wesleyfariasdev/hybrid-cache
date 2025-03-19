d1046b9896900b4fd45ae950f5e3d1093b7477f5
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-18 21:41:50 -0300

    implementa endpoints com operações crud para o cache

--- HybridCache.Api/Controllers/WeatherForecastController.cs
+++ HybridCache.Api/Controllers/WeatherForecastController.cs
@@ -1,31 +1,92 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text.Json;
 
-namespace HybridCache.Api.Controllers;
+namespace HybridCacheDotnet9.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    [HttpGet("MemoryCache")]
+    public IEnumerable<WeatherForecast> GetMemoryCache([FromServices] IMemoryCache memoryCache)
     {
-        _logger = logger;
+        if (!memoryCache.TryGetValue("WeatherForecast", out IEnumerable<WeatherForecast> forecasts))
+        {
+            forecasts = GetData();
+            memoryCache.Set("WeatherForecast", forecasts, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
+            });
+        }
+        return forecasts;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("DistributedCache")]
+    public async Task<IEnumerable<WeatherForecast>> GetDistributedCache([FromServices] IDistributedCache distributedCache)
+    {
+        var forecastsString = await distributedCache.GetStringAsync("WeatherForecast");
+        if (!string.IsNullOrWhiteSpace(forecastsString))
+            return JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(forecastsString)!;
+
+        var forecasts = GetData();
+
+        await distributedCache.SetStringAsync("WeatherForecast",
+            JsonSerializer.Serialize(forecasts),
+            new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+            });
+
+        return forecasts;
+    }
+
+    [HttpGet("GetOrCreate_HybridCache")]
+    public async Task<IEnumerable<WeatherForecast>> GetHybridCache([FromServices] HybridCache hybridCache)
+    {
+        return await hybridCache.GetOrCreateAsync("WeatherForecast",
+            cancellationToken => ValueTask.FromResult(GetData()),
+            new HybridCacheEntryOptions
+            {
+                Expiration = TimeSpan.FromSeconds(10),
+                LocalCacheExpiration = TimeSpan.FromSeconds(10),
+                Flags = HybridCacheEntryFlags.DisableDistributedCache
+            },
+            new[] { "tag1", "tag2" });
+    }
+
+    [HttpGet("Set_HybridCache")]
+    public async Task SetHybridCache([FromServices] HybridCache hybridCache)
+    {
+        await hybridCache.SetAsync("WeatherForecast",
+            GetData(),
+            new HybridCacheEntryOptions
+            {
+                Expiration = TimeSpan.FromMinutes(10),
+                LocalCacheExpiration = TimeSpan.FromMinutes(10),
+            },
+            new[] { "tag1", "tag2" });
+    }
+
+    [HttpGet("Remove_HybridCache")]
+    public async Task RemoveHybridCache([FromServices] HybridCache hybridCache)
+    {
+        await hybridCache.RemoveAsync("WeatherForecast");
+
+        //Remove by tag
+        //await hybridCache.RemoveByTagAsync("tag1");
+    }
+
+    private IEnumerable<WeatherForecast> GetData()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Temperature = Random.Shared.Next(-20, 50),
+            Humidity = Random.Shared.Next(5, 100)
         })
         .ToArray();
     }
--- HybridCache.Api/WeatherForecast.cs
+++ HybridCache.Api/WeatherForecast.cs
@@ -1,13 +1,10 @@
-namespace HybridCache.Api
+namespace HybridCacheDotnet9.Api;
+
+public class WeatherForecast
 {
-    public class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
+    public DateOnly Date { get; set; }
 
-        public int TemperatureC { get; set; }
+    public int Temperature { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-    }
+    public int Humidity { get; set; }
 }
