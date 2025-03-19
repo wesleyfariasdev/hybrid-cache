a6bd287f5626b830e20f720a671ce7cbfcd6a68e
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-03-18 21:03:30 -0300

    configuração do cache hibrido

--- HybridCache.Api/Program.cs
+++ HybridCache.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Caching.Hybrid;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// cache em memória
+builder.Services.AddMemoryCache();
+
+// redis: DB 
+builder.Services.AddStackExchangeRedisCache(options =>
+ {
+     options.Configuration = "localhost:6379";
+ });
+
+// recurso de cache hibrido
+builder.Services.AddHybridCache(options =>
+{
+    options.MaximumPayloadBytes = 1024 * 1024;
+    options.MaximumKeyLength = 1024;
+    options.DefaultEntryOptions = new HybridCacheEntryOptions
+    {
+        Expiration = TimeSpan.FromMinutes(5),
+        LocalCacheExpiration = TimeSpan.FromMinutes(5),
+    };
+});
 
 var app = builder.Build();
 
